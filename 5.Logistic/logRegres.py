20bc9f4899f55999f36c04ca4a62095718ff47c8
rising <625687551@qq.com>
2018-06-25 07:51:23 +0800

    change name

--- 5.Logistic/logRegres.py
+++ 5.Logistic/logRegres.py
@@ -2,8 +2,6 @@
 
 import random
 import numpy as np
-import pandas as pd
-
 
 
 def loadDataSet():
@@ -18,7 +16,7 @@ def loadDataSet():
 
 
 def sigmoid(inX):
-    return 1.0/(1+np.exp(-inX))
+    return 1.0 / (1 + np.exp(-inX))
 
 
 def gradAscent(dataMatIn, classLabels):
@@ -29,7 +27,7 @@ def gradAscent(dataMatIn, classLabels):
     maxCycle = 500
     weights = np.ones((n, 1))
     for k in range(maxCycle):
-        h = sigmoid(dataMatrix*weights)
+        h = sigmoid(dataMatrix * weights)
         error = (labelMat - h)
         weights = weights + alpha * dataMatrix.transpose() * error
     return weights
@@ -41,12 +39,12 @@ def stocGradAscent0(dataMatrix, classLabels, numIter=150):
     for j in range(numIter):
         dataIndex = [x for x in range(m)]
         for i in range(m):
-            alpha = 4/(1.0 + j + i) + 0.0001
+            alpha = 4 / (1.0 + j + i) + 0.0001
             randIndex = int(random.uniform(0, len(dataIndex)))
-            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
             error = classLabels[randIndex] - h
-            weights =  weights +alpha*error*dataMatrix[randIndex]
-            del(dataIndex[randIndex])
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del (dataIndex[randIndex])
     return weights
 
 
@@ -70,47 +68,56 @@ def plotBestFit(weights):
     ax.scatter(xcord1, ycord1, s=30, c="red", marker="s")
     ax.scatter(xcord2, ycord2, s=30, c="green")
     x = np.arange(-3.0, 3.0, 0.1)
-    y = (-weights[0]-weights[1]*x)/weights[2]
+    y = (-weights[0] - weights[1] * x) / weights[2]
     ax.plot(x, y)
     plt.xlabel("x1")
     plt.ylabel("x2")
     plt.show()
 
+
 def classifyVector(inX, weights):
-    prob = sigmoid(sum(inX*weights))
-    if prob > 0.5: return 1.0
-    else: return 0.0
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
 
 
 def colicTest():
-    frTrain = open('horseColicTraining.txt'); frTest = open('horseColicTest.txt')
-    trainingSet = []; trainingLabels = []
+    frTrain = open('horseColicTraining.txt');
+    frTest = open('horseColicTest.txt')
+    trainingSet = [];
+    trainingLabels = []
     for line in frTrain.readlines():
         currLine = line.strip().split('\t')
-        lineArr =[]
+        lineArr = []
         for i in range(21):
             lineArr.append(float(currLine[i]))
         trainingSet.append(lineArr)
         trainingLabels.append(float(currLine[21]))
     trainWeights = stocGradAscent0(np.array(trainingSet), trainingLabels, 1000)
-    errorCount = 0; numTestVec = 0.0
+    errorCount = 0;
+    numTestVec = 0.0
     for line in frTest.readlines():
         numTestVec += 1.0
         currLine = line.strip().split('\t')
-        lineArr =[]
+        lineArr = []
         for i in range(21):
             lineArr.append(float(currLine[i]))
-        if int(classifyVector(np.array(lineArr), trainWeights))!= int(currLine[21]):
+        if int(classifyVector(np.array(lineArr), trainWeights)) != int(currLine[21]):
             errorCount += 1
-    errorRate = (float(errorCount)/numTestVec)
+    errorRate = (float(errorCount) / numTestVec)
     print("the error rate of this test is: %f" % errorRate)
     return errorRate
 
+
 def multiTest():
-    numTests = 200; errorSum=0.0
+    numTests = 200;
+    errorSum = 0.0
     for k in range(numTests):
         errorSum += colicTest()
-    print("after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests)))
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests)))
+
 # dataArr, labelMat = loadDataSet()
 # weights = gradAscent(dataArr, labelMat)
 # weights = stocGradAscent0(np.array(dataArr), labelMat)
---
+++ test.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+
+from sklearn.linear_model import LogisticRegression
